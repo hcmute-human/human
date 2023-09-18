e26f9f11227556232d2ccdd9a1583639944cf8f1
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-09-19 01:55:23 +0700

    feat: configure PersistenceOptions

--- src/Human.Web/Program.cs
+++ src/Human.Web/Program.cs
@@ -1,12 +1,26 @@
-public class Program
+using Human.Core.Models;
+
+namespace Human.Web;
+
+public static class Program
 {
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        Configure(builder.Services, builder.Configuration);
+
         var app = builder.Build();
-
-        app.MapGet("/", () => "Hello World!");
-
         app.Run();
+    }
+
+    private static void Configure(IServiceCollection services, IConfiguration configuration)
+    {
+        var persistenceSection = configuration.GetRequiredSection(PersistenceOptions.Section);
+        services.AddOptions<PersistenceOptions>()
+            .Bind(persistenceSection)
+            .ValidateOnStart()
+            .ValidateDataAnnotations();
+
+        services.AddPersistence(persistenceSection.Get<PersistenceOptions>()!);
     }
 }
