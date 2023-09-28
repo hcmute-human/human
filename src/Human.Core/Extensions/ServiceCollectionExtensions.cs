d53df0fb44c437bfa02eaeada1da03445300e74f
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-09-28 20:35:49 +0700

    feat: add email template renderer service

--- src/Human.Core/Extensions/ServiceCollectionExtensions.cs
+++ src/Human.Core/Extensions/ServiceCollectionExtensions.cs
@@ -8,6 +8,7 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddCore(this IServiceCollection self)
     {
         self.AddSingleton<IJwtBearerService, JwtBearerService>();
+        self.AddSingleton<IEmailTemplateRenderer, HtmlEmailTemplateRenderer>();
         return self;
     }
 }
---
+++ src/Human.Core/Interfaces/IEmailTemplateRenderer.cs
@@ -0,0 +1,6 @@
+﻿namespace Human.Core.Interfaces;
+
+public interface IEmailTemplateRenderer
+{
+    Task<string> RenderAsync<T>(string templateKey, T model, CancellationToken ct = default);
+}
