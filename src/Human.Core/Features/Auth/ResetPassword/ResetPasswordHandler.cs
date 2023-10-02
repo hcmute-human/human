6bcfe0a1883a447fc6c0232df00bc78ab54a84c7
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-10-02 14:42:28 +0700

    feat: check if token expired

--- src/Human.Core/Features/Auth/ResetPassword/ResetPasswordHandler.cs
+++ src/Human.Core/Features/Auth/ResetPassword/ResetPasswordHandler.cs
@@ -3,6 +3,7 @@ using FastEndpoints;
 using FluentResults;
 using Human.Core.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using NodaTime;
 
 namespace Human.Core.Features.Auth.ResetPassword;
 
@@ -17,11 +18,11 @@ public sealed class ResetPasswordHandler : ICommandHandler<ResetPasswordCommand,
 
     public async Task<Result<ResetPasswordResult>> ExecuteAsync(ResetPasswordCommand command, CancellationToken ct)
     {
-        var user = await dbContext.UserPasswordResetTokens.Where(x => x.Token == command.Token)
-            .Select(x => new { x.User.Id })
+        var token = await dbContext.UserPasswordResetTokens.Where(x => x.Token == command.Token)
+            .Select(x => new { UserId = x.User.Id, x.ExpirationTime })
             .FirstOrDefaultAsync(cancellationToken: ct)
             .ConfigureAwait(false);
-        if (user is null)
+        if (token is null)
         {
             return Result.Fail("Token does not exist")
                 .WithName(nameof(command.Token))
@@ -29,13 +30,21 @@ public sealed class ResetPasswordHandler : ICommandHandler<ResetPasswordCommand,
                 .WithStatus(HttpStatusCode.BadRequest);
         }
 
-        var hash = BCrypt.Net.BCrypt.EnhancedHashPassword(command.Password);
         await dbContext.UserPasswordResetTokens
             .Where(x => x.Token == command.Token)
             .ExecuteDeleteAsync(ct)
             .ConfigureAwait(false);
+        if (SystemClock.Instance.GetCurrentInstant() > token.ExpirationTime)
+        {
+            return Result.Fail("Token has already expired")
+                .WithName(nameof(command.Token))
+                .WithCode("token_expired")
+                .WithStatus(HttpStatusCode.BadRequest);
+        }
+
+        var hash = BCrypt.Net.BCrypt.EnhancedHashPassword(command.Password);
         await dbContext.Users
-            .Where(x => x.Id == user.Id)
+            .Where(x => x.Id == token.UserId)
             .ExecuteUpdateAsync(u => u.SetProperty(x => x.PasswordHash, hash), ct)
             .ConfigureAwait(false);
         return new ResetPasswordResult();
